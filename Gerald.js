f5cc8dce61f5c5bbff162ff7f321b2347cc547ab
Cade Brown <cadebrown1776@gmail.com>
2021-03-23 15:20:38 -0700

    modified:   Gerald.js ,	deleted:    index.js ,	deleted:    personal_resources/safe.txt ,	new file:   responses.json

--- Gerald.js
+++ Gerald.js
@@ -4,6 +4,7 @@ const Gerald = new Discord.Client();
 const config = require('./config.json');
 const prefix = config.prefix;
 const token = config.token;
+const responses = require('./responses.json')
 Gerald.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file=>file.endsWith('.js'));
 for(const file of commandFiles){
@@ -23,28 +24,18 @@ Gerald.on('message', message=>{
         message.channel.send(`test`);
         fs.appendFileSync('./usage_logs/usageLogs.txt',`${message.author.tag} called the test function\n------------------------------------------------------------------------\n`);
     };//used for testing
-    if(message.content==(`${prefix}`)){};//used for testing if different commands work
+    if(message.content==(`${prefix}insult me`)){
+        let temp = Math.round((Math.random()*5)-1);
+        if(temp <0){temp=0};
+        message.channel.send(`Hey ${message.author}`);
+        setTimeout(()=>{message.channel.send(`${responses.responses.insults[temp]}`);},1000);
+    };//used for testing if different commands work
 });
 
 /*
 ===================================================================================================================
 Notebook===========================================================================================================
-
-
 Basic command structure ===>    if(message.content==(`${prefix}`)){};
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+----Create an array of insults
 ====================================================================================================================*/
