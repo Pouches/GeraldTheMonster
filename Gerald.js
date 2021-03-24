fd2eb98879f45fb643dac71a4de43db0a651e711
Cade Brown <cadebrown1776@gmail.com>
2021-03-24 15:22:23 -0700

    modified:   Gerald.js ,	modified:   responses.json ,	modified:   usage_logs/usageLogs.txt

--- Gerald.js
+++ Gerald.js
@@ -4,7 +4,9 @@ const Gerald = new Discord.Client();
 const config = require('./config.json');
 const prefix = config.prefix;
 const token = config.token;
-const responses = require('./responses.json')
+const responses = require('./responses.json');
+const tanks = responses.responses.tanks;
+const tankList = tanks.list;
 Gerald.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file=>file.endsWith('.js'));
 for(const file of commandFiles){
@@ -28,10 +30,26 @@ Gerald.on('message', message=>{
         let temp = Math.round((Math.random()*5)-1);
         if(temp <0){temp=0};
         message.channel.send(`Hey ${message.author}`);
+        fs.appendFileSync('./usage_logs/usageLogs.txt',`${message.author.tag} called the insult function\n------------------------------------------------------------------------\n`);
         setTimeout(()=>{message.channel.send(`${responses.responses.insults[temp]}`);},1000);
     };//used for testing if different commands work
+    if(message.content ==(`${prefix}tank info`)){
+        let temp = 0;
+        let finalmessage="";
+        message.channel.send("SHHHHH, Cade might hear you and no one wants to talk to him");
+        message.channel.send(`Which one do you want to talk about?\n`);
+        tankList.forEach(element => {
+            finalmessage=finalmessage.concat(`\n[${temp}]:${element}`);
+            temp++;
+        });
+        message.channel.send(`=========================================${finalmessage}\n=========================================\n please replay with only the corresponding number`);
+        if(message.content){
+            let temp = parseInt(message.content);
+            let tank = `${tankList[temp]}`;
+            Gerald.on('message',message2=>{message2.channel.send(tanks.temp)})
+        }
+    }
 });
-
 /*
 ===================================================================================================================
 Notebook===========================================================================================================
