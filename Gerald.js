f1b45f897dd1038bc4fc365aa29617522272a089
Cade Brown <cadebrown1776@gmail.com>
2021-03-24 20:55:04 -0700

    modified:   Gerald.js ,	modified:   package-lock.json ,	modified:   package.json ,	modified:   responses.json ,	modified:   usage_logs/usageLogs.txt

--- Gerald.js
+++ Gerald.js
@@ -6,7 +6,8 @@ const prefix = config.prefix;
 const token = config.token;
 const responses = require('./responses.json');
 const tanks = responses.responses.tanks;
-const tankList = tanks.list;
+const tankList = [tanks.M4,tanks['Panzer 4'],tanks['Tiger 1'],tanks.Panther,tanks.M18,tanks.M36,tanks['T-34'],tanks['KV-1'],tanks.M4A3E2];
+const List = tanks.list;
 Gerald.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file=>file.endsWith('.js'));
 for(const file of commandFiles){
@@ -33,21 +34,27 @@ Gerald.on('message', message=>{
         fs.appendFileSync('./usage_logs/usageLogs.txt',`${message.author.tag} called the insult function\n------------------------------------------------------------------------\n`);
         setTimeout(()=>{message.channel.send(`${responses.responses.insults[temp]}`);},1000);
     };//used for testing if different commands work
-    if(message.content ==(`${prefix}tank info`)){
+    if(message.content ==(`${prefix}tank info`)){//a command for displaying tank info
         let temp = 0;
         let finalmessage="";
         message.channel.send("SHHHHH, Cade might hear you and no one wants to talk to him");
         message.channel.send(`Which one do you want to talk about?\n`);
-        tankList.forEach(element => {
+        List.forEach(element => {
             finalmessage=finalmessage.concat(`\n[${temp}]:${element}`);
             temp++;
         });
-        message.channel.send(`=========================================${finalmessage}\n=========================================\n please replay with only the corresponding number`);
-        if(message.content){
-            let temp = parseInt(message.content);
-            let tank = `${tankList[temp]}`;
-            Gerald.on('message',message2=>{message2.channel.send(tanks.temp)})
-        }
+        message.channel.send(`=========================================${finalmessage}\n=========================================\n please replay with only <tank (number)`);
+        // setTimeout(()=>{
+        //     try{
+        //     if(message.content!=finalmessage){
+        //         let tank = `${tankList[parseInt(message.content)]}`;
+        //         message.channel.send(tank.Nickname);
+        //     }}catch(err){console.log(err)};
+        // },5000);
+    }
+    if(message.content.startsWith(`${prefix}tank `)){
+        let selected = tankList[message.content.slice(6)];
+        message.channel.send(`${selected.name}\nNickname(s):${selected.Nickname}\nArmament within the mantlet:${selected.Armament.turret.mantlet}\nArmor(front/side):${selected.Armor['Upper Front plate']}/${selected.Armor['hull side']}\n Top speed:${selected['Top Speed']}`)
     }
 });
 /*
